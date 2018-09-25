8a22f391a51b83c8818137e9801b3d6a7a3cbc68
be-desarrollo <desarrollo@bigelephant.mx>
2018-09-25 17:33:43 -0500

    U[NOSOTROS, CASAS] Se actualizo la vista de Casas, y se hizo los ajustes en la seccion de Nosotros para que fuera responsive la seccion

--- nosotros.php
+++ nosotros.php
@@ -1,10 +1,10 @@
-<?php include 'header.php';?>	
+<?php include 'header.php';?>
 
 	<div class="header-img">
 		<img src="img/bg_nosotros.jpg" class="img-responsive">
 	</div>
 	<div class="sp10x"></div>
-	
+
 	<div class="container">
     	<div class="row">
 			<div class="col-sm-24 col-xs-24 col-md-24 col-lg-24 text-center">
@@ -12,15 +12,15 @@
 				<h2 class="tintC"><b>Misión</b></h2>
 				<div class="sp2x"></div>
 				<p class="fnt15"><b>Posicionarnos como una empresa líder en el sector inmobiliario y de la construcción</b><br>
-				<span class="tintC">en el proceso de aumento del patrimonio de nuestros clientes, por medio de una atención personalizada durante la compra-venta de bienes inmuebles que les lleve a la toma de mejores decisiones.</span> </p>		
+				<span class="tintC">en el proceso de aumento del patrimonio de nuestros clientes, por medio de una atención personalizada durante la compra-venta de bienes inmuebles que les lleve a la toma de mejores decisiones.</span> </p>
 				<div class="sp6x"></div>
 				<h2 class="tintC"><b>Visión</b></h2>
 				<div class="sp2x"></div>
 				<p class="fnt15"><b>Ofrecer un servicio integral brindando asesoría financiera y legal</b><br>
-				<span class="tintC">con presencia en todo el territorio nacional brindando las mejores opciones de inversión a nuestros clientes a través de procesos claros, confiables y sencillos. Residencia ubicada en condominio la Rioja, Tlajomulco de Zuñiga, 4 niveles,3 recamaras, 4 baños, cuarto de servicio,roof garden Descripcion Descripcion Email recepcion@acego.com.mx</span> </p>		
+				<span class="tintC">con presencia en todo el territorio nacional brindando las mejores opciones de inversión a nuestros clientes a través de procesos claros, confiables y sencillos. Residencia ubicada en condominio la Rioja, Tlajomulco de Zuñiga, 4 niveles,3 recamaras, 4 baños, cuarto de servicio,roof garden Descripcion Descripcion Email recepcion@acego.com.mx</span> </p>
 				<div class="sp10x"></div>
 
-				<div class="col-sm-24 col-xs-12 col-md-6 col-lg-6">
+				<div class="col-sm-12 col-xs-24 col-md-6 col-lg-6">
 					<div class="box-nosotros">
 						<img src="img/icn_service.svg" class="img-responsive">
 						<span>Servicio</span>
@@ -29,28 +29,28 @@
 						los clientes, por entender sus
 						necesidades y satisfacerlas
 						con rapidez y eficacia.</p>
-					</div>	
+					</div>
 				</div>
-				<div class="col-sm-24 col-xs-12 col-md-6 col-lg-6">
+				<div class="col-sm-12 col-xs-24 col-md-6 col-lg-6">
 					<div class="box-nosotros">
 						<img src="img/icn_honestidad.svg" class="img-responsive">
 						<span>Honestidad</span>
 						<p>Por medio de la transparencia en
-						nuestros procesos y claridad en la 
-						comunicación estableciendo metas 
+						nuestros procesos y claridad en la
+						comunicación estableciendo metas
 						alcanzables y especificas.</p>
-					</div>	
+					</div>
 				</div>
-				<div class="col-sm-24 col-xs-12 col-md-6 col-lg-6">
+				<div class="col-sm-12 col-xs-24 col-md-6 col-lg-6">
 					<div class="box-nosotros">
 						<img src="img/confianza.svg" class="img-responsive">
 						<span>Confianza</span>
-						<p>Generar relaciones comerciales basadas 
+						<p>Generar relaciones comerciales basadas
 							en la honestidad y el cumplimiento
 							 de nuestros compromisos</p>
-					</div>	
+					</div>
 				</div>
-				<div class="col-sm-24 col-xs-12 col-md-6 col-lg-6">
+				<div class="col-sm-12 col-xs-24 col-md-6 col-lg-6">
 					<div class="box-nosotros">
 						<img src="img/icn_valor.svg" class="img-responsive">
 						<span>Valor agregado</span>
@@ -60,9 +60,9 @@
 						alto nivel de respuesta a
 						las inquietudes de
 						nuestros clientes.</p>
-					</div>	
+					</div>
 				</div>
-				<div class="clearfix"></div>	
+				<div class="clearfix"></div>
 			</div><!--col24-->
 
 		</div><!--row-->
